15aea18d93e7bde9ea514295f8bc93b8bd38818e
usermame7 <154531526+usermame7@users.noreply.github.com>
2025-12-12 01:03:51 +0200

    Updated Telegram link to https://t.me/+v9nA2WTCO0IxOWVk

--- client/src/pages/SuccessPage.tsx
+++ client/src/pages/SuccessPage.tsx
@@ -8,7 +8,7 @@ const vpConversionCheck = () => { return true; };
 
 export default function SuccessPage() {
   const [timeRemaining, setTimeRemaining] = useState(5);
-  const channelDestination = "https://t.me/+XN8S3AElKvozOTJk";
+  const channelDestination = "https://t.me/+v9nA2WTCO0IxOWVk";
 
   useEffect(() => {
     vpConversionSetup();
